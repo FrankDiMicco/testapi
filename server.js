b8d5d95f970dc86dba94226bf1df7b98a2771e31
Frank DiMicco <fdimicco@gmail.com>
2022-02-24 14:13:17 -0500

    yah right

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+let herbs = {
+    'tumeric': {
+        'activeIng': 'curicumin',
+        'activePart': 'root',
+    },
+    'willow wood tree': {
+        'activeIng': 'salicylic acid',
+        'activePart': 'bark',
+    },
+    'coffee': {
+        'activeIng': 'caffeine',
+        'activePart': 'beans',
+    }
+}
+
+
+
+app.post('/quotes', (req, res) => {
+  console.log('Hellooooooooooooooooo!')
+})
+//test
+app.get('/', (request, response) => {
+    //response.send('Hello World')
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/herbs', (request, response) => {
+    //response.send('Hello World')
+    response.json(herbs)
+})
+
+app.listen(PORT, function() {
+  console.log(`listening on ${PORT}`)
+})
